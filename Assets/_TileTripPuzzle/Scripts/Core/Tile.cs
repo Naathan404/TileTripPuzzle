7fe033299f21bd56afb76f48c0b6d7da730ce670
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-05-28 00:44:41 +0700

    chore: set up project, download DoTween

---
+++ Assets/_TileTripPuzzle/Scripts/Core/Tile.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private int _x;
+    [SerializeField] private int _y;
+    [SerializeField] private bool _isBlocked;
+
+
+}
